5cf82016c6f1166700b5eae5b6ae45e35ef9d0ac
ankit <ankitkumarswe01@gmail.com>
2026-03-02 19:30:28 +0530

    updated

---
+++ index19a.js
@@ -0,0 +1,30 @@
+
+import express from "express"
+const app = express();
+const num1 = process.argv[2];
+const num2 = process.argv[3];
+const PORT = process.agrv[2];
+
+app.listen(PORT, ()=>{
+    console.log("server is running");
+})
+
+app.get("/", (req, res)=>{
+    res.send(`the sum of ports is ${num1+num2}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
